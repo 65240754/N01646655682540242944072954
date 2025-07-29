bed9d873dfcf385924c74e03b5f779111200d0a9
65240754 <darkcrazy98@hotmail.com>
2025-07-29 06:01:00 -0600

    github_token.sh

--- github_token.sh
+++ github_token.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 
 # === CONFIGURACIÓN ===
-REPO="AOHH9803252Q5/demo-repository"
+REPO="/65240754/N01646655682540242944072954/"
 GITHUB_TOKEN="tu_token_personal"
 USER_AGENT="bot-subida"
 ARCHIVO="/ruta/a/temporal/archivo_codificado.txt"
@@ -25,7 +25,7 @@ curl -X PUT \
   -H "Authorization: token $GITHUB_TOKEN" \
   -H "User-Agent: $USER_AGENT" \
   -H "Accept: application/vnd.github.v3+json" \
-  https://api.github.com/repos/$REPO/contents/$NOMBRE_ARCHIVO_EN_REPO \
+  https://api.github.com/repos/$REPO/contents/$N01646655682540242944072954 \
   -d @payload.json
 exit
 ```
